c4a40e2477fb4e5a360796ec4ab3d824022119f2
Kaio Paranhos <131325721+kaioparanhos@users.noreply.github.com>
2025-02-14 00:03:21 -0300

    Create foxhound-scan.go

---
+++ scanner/foxhound-scan.go
@@ -0,0 +1,20 @@
+package main
+
+// lib
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"sort"
+	"sync"
+	"time"
+
+	"github.com/likexian/whois" // lib
+)
+
