ef405278c764661b35ca794d8e702ed79eed7733
Kaio Paranhos <131325721+kaioparanhos@users.noreply.github.com>
2025-02-15 17:26:05 -0300

    Update foxhound-scan.go

--- scanner/foxhound-scan.go
+++ scanner/foxhound-scan.go
@@ -36,10 +36,6 @@ type Config struct {
 type Result struct {
 
 	IP        string    `json:"ip"`
-	Porta     int    	`json:"porta"`
-	Status    string 	`json:"status"`
-	Servico   string 	`json:"servico,omitempty"`
-	Mensagem  string 	`json:"mensagem,omitempty"`
 	Porta     int       `json:"porta"`
 	Status    string    `json:"status"`
 	Servico   string    `json:"servico,omitempty"`
